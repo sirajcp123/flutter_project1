3071f5fed59940372ae92d033cc628d64d156205
Rashid.v <rashidrashi0004321@gmail.com>
2024-03-07 13:34:21 +0530

    main.dart changes

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "",
+      title: "Hello",
       home:ViewScreen(),
     );
   }
